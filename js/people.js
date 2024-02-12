db550c193ee4882b19d523aaa5dc5bbf22e81094
jillian-milton <jillianmilton@yahoo.com>
2024-02-12 17:30:01 -0500

    started search people

---
+++ js/people.js
@@ -0,0 +1,7 @@
+let hamburger = document.querySelector('.hamburger');
+let open = document.querySelector('.open');
+let closed = document.querySelector('.closed');
+
+hamburger.addEventListener('click', function(){
+    open.classList.toggle('closed');
+})
